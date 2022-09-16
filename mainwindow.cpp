06381f2a139dd5ce01785b334b300e064c347048
Petr5 <petr.mavzovin@gmail.com>
2022-09-17 02:29:02 +0300

    add messageBox, click buttom and horizontal scroll bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->horizontalScrollBar,SIGNAL(valueChanged(int)), ui->progressBar, SLOT(setValue(int)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("Status: On");
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "Title", "QT doesn't support", QMessageBox::YesAll | QMessageBox::Yes);
+    if (reply == QMessageBox::YesAll) {
+        QApplication::quit();
+    }
+    else{
+        qDebug() << "No is clicked";
+    }
 }
